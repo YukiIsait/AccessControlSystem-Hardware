dff8c11c6b1ef5acb1b43b62b9fcb1d18a34b312
YukiIsait <yukiisait@outlook.jp>
2024-12-28 21:55:53 +0800

    Feat: 移除未使用的IO占用

--- src/Key.cc
+++ src/Key.cc
@@ -1,8 +1,8 @@
 #include "Key.h"
 
 void Key::begin() {
-    pinMode(PB4, OUTPUT);
-    digitalWrite(PB4, LOW);
+    // pinMode(PB4, OUTPUT);
+    // digitalWrite(PB4, LOW);
     pinMode(PB3, INPUT_PULLUP);
 }
 
